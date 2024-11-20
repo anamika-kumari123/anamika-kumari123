5a99c7ec19ca94b5d17178345a750eaec493c4b4
anamika-kumari123 <anamika.001keshri@gmail.com>
2024-11-20 23:15:19 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,5 +1,5 @@
 def read_file():
-    with open(r"C:\backup\Storage_qualification\ust\anamika-kumari123\passwords.txt","r") as file1:
+    with open(r"passwords.txt","r") as file1:
         # print(file1.read().split("\n"))
         out = file1.read().split("\n")
         return out
